344c67d5ca3e3dc9c68ef93618202d210bb74380
Cebastion <kdimon2006@gimal.com>
2024-01-01 22:12:49 +0200

    new commit

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,8 +1,24 @@
-import { FC, useState } from 'react'
+'use client'
+
+import { FC, useState, useEffect } from 'react'
 import style from './header.module.scss'
 import { IActiveBurger } from '@/interface/ActiveBurger.interface'
 
 const Header: FC<IActiveBurger> = ({Active, SetActive}) => {
+  const BreakPoint = 575
+  const [screenWidth, SetScreenWidth] = useState(0)
+
+  useEffect(() => {
+    const updateScreenWidth = () => {
+      SetScreenWidth(window.innerWidth)
+    }
+    updateScreenWidth();
+    window.addEventListener('resize', updateScreenWidth)
+    return () => {
+      window.removeEventListener('resize', updateScreenWidth)
+    }
+  }, [])
+
   return (
     <header className={style.header}>
       <div className={style.header__container}>
@@ -10,7 +26,7 @@ const Header: FC<IActiveBurger> = ({Active, SetActive}) => {
           <img src="/logo.webp" alt="logo" />
         </div>
         <div className={style.header__title}>
-          <h1>Путівник пам’яті вулицями Кам’янського</h1>
+          <h1>{screenWidth === BreakPoint ? 'Путівник пам’яті вулицями Кам’янського' : 'ППВК'}</h1>
         </div>
         <div className={Active ? `${style.burger} ${style.active}` : style.burger} onClick={() => SetActive(!Active)}>
           <span></span>
